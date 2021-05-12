38fc1a44d1b1c7e05edbc44519bf55c40e3c2324
Neon <51766327+DeveloperNeon@users.noreply.github.com>
2021-05-12 15:17:46 -0500

    more inputs
    more code

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import smtplib
 from email.message import EmailMessage
 
 amount = input('How many messages to send (enter loop to send infinite): ')
+addimage = input('Would you like to add an image? (yes/no) If you choose to add an image, You need to have a file in this directory named img.jpg: ')
+emails = input('Please enter the email you would like to spam: ')
+message = input('Please enter the message content: ')
 
 def email_alert(content, to):
   msg = EmailMessage()
@@ -18,11 +21,12 @@ def email_alert(content, to):
   server = smtplib.SMTP("smtp.gmail.com", 587)
   server.starttls()
   server.login(user, password)
-
+  
+  if(addimage=='yes'|'y'){
   with open('img.jpg', 'rb') as content_file:
     content = content_file.read()
     msg.add_attachment(content, maintype='application', subtype='jpg', filename='img.jpg')
-
+  }
   server.send_message(msg)
 
   server.quit()
@@ -33,15 +37,11 @@ if(type(amount)=='int'){
   while i > amount:
     time.sleep(0.25)
     print("sent to: ", random.choice(users))
-    email_alert("Message", random.choice(users))
+    email_alert(message, random.choice(users))
 } else {
-  print('you either typed loop or left the message count number blank, Sending infinite messages.')
+  print('you either typed loop or left the message count number blank, or typed an invalid answer. Sending infinite messages.')
   while True:
     time.sleep(0.25)
-    print("sent to: ", random.choice(users))
-    email_alert("Message", random.choice(users))
-
-while True:
-  time.sleep(0.25)
-  print("sent to: ", random.choice(users))
-  email_alert("Message", random.choice(users))
+    print("sent to: ", random.choice(emails))
+    email_alert(message, random.choice(emails))
+}
